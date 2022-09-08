44d20fa1c6c73484e6d1c5be36a5e291591e5e5f
Renan Araujo <ra.carvalho@unesp.br>
2022-09-08 19:19:27 -0300

    create questions table

---
+++ prisma/migrations/20220908221848_create_questions_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "questions" (
+    "id" SERIAL NOT NULL,
+    "askedBy" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id")
+);
